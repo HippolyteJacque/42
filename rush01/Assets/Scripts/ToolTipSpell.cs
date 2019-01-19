abb18f658f39cef8d56833df0d134b4674fce517
unknown <hippolytejacque@hotmail.fr>
2019-01-19 21:49:28 +0100

    tooltips spells

---
+++ rush01/Assets/Scripts/ToolTipSpell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipSpell : MonoBehaviour {
+
+	public CanvasGroup mySpellCanvas;
+	//public TextMesh mySpellTip;
+	//public GameObject mySpell;
+
+ 	public void DisplaySpellTip(){
+ 		mySpellCanvas.alpha = 1f;
+ 	}
+
+ 	public void HideSpellTip(){
+		mySpellCanvas.alpha = 0f;
+ 	}
+}
